c1982bc035ea0606b82ec756eeba7d780bf69366
Yao Song <yao.s.1216@gmail.com>
2021-08-18 20:30:36 -0700

    last questions

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) =>{
     res.send('Welcome to Root!');
 })
 
+app.get('/aboutMe', function(req, res) {   
+    console.log("about Me request");
+    res.send('Welcome to About Me!');
+ })
+
 app.listen(port, ()=>{
     console.log(`Final Exam listening at http://localhost:${port}`);
 })
+
+
+
